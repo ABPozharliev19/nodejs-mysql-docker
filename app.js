adc13565c9350997466964ad33d9c918f3bad995
Atanas Pozharliev <ABPozharliev19@codingburgas.bg>
2021-12-25 20:22:00 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mysql = require("mysql2")
+const app = express();
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    port: 3306
+});
+
+connection.connect((err) => {
+    if(err) {
+        console.log(err);
+        return;
+    }
+    console.log("POOOOG");
+})
+
+const PORT = process.env.PORT || 4000;
+
+app.get('/', (req, res) => {
+    res.send("pog");
+})
+
+app.listen(PORT, () => {
+    console.log("Server is listening, pog");
+})
